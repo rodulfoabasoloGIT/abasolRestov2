65da3604f4c8cccff24cc51b85ee1384e8f7d6c2
Abasol <ramzeeesss666@gmail.com>
2024-03-18 14:31:17 +0800

    changes on menu page

--- menu.js
+++ menu.js
@@ -2,10 +2,42 @@ const rightBtn = document.querySelector(".right-btn");
 
 const leftBtn = document.querySelector(".left-btn");
 
+// buttons
 rightBtn.addEventListener("click", () => {
   document.querySelector(".menu-selection").scrollLeft += 500;
 });
 
 leftBtn.addEventListener("click", () => {
   document.querySelector(".menu-selection").scrollLeft -= 500;
+});
+//buttons
+
+// document.querySelector(".beef").addEventListener("click", async function (e) {
+//   const variable = e.currentTarget.classList[1];
+//   async function logMovies() {
+//     const response = await fetch(
+//       `https://www.themealdb.com/api/json/v1/1/filter.php?c=${variable}`
+//     );
+//     const movies = await response.json();
+//     console.log(movies);
+//   }
+//   logMovies();
+// });
+
+const test = document.querySelectorAll(".img-contain");
+
+test.forEach((e) => {
+  e.addEventListener("click", (e) => {
+    const variable = e.currentTarget.classList[1];
+    async function logMovies() {
+      const response = await fetch(
+        `https://www.themealdb.com/api/json/v1/1/filter.php?c=${variable}`
+      );
+      const movies = await response.json();
+      console.log(movies);
+    }
+    logMovies();
+
+    document.querySelector(".result").innerText = variable;
+  });
 });
