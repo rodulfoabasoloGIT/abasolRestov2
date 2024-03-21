332a6d8a510fb7ee12f8ae78ed35cd912647d1fb
Abasol <ramzeeesss666@gmail.com>
2024-03-21 15:25:08 +0800

    categories content added

--- menu.js
+++ menu.js
@@ -24,21 +24,60 @@ leftBtn.addEventListener("click", () => {
 //   logMovies();
 // });
 
-const test = document.querySelectorAll(".img-contain");
+const categoriesMenu = document.querySelectorAll(".img-contain");
 
-test.forEach((e) => {
+// categoriesMenu.forEach((e) => {
+//   e.addEventListener("click", (e) => {
+//     const variable = e.currentTarget.classList[1];
+//     async function logMovies() {
+//       const response = await fetch(
+//         `https://www.themealdb.com/api/json/v1/1/filter.php?c=${variable}`
+//       );
+//       const movies = await response.json();
+//       const renderedData = movies.meals.slice(0, 10);
+
+//       const categChoices = document.createElement("div");
+//       categChoices.classList.add("categ-choise");
+
+//       categChoices.appendChild(renderedData[0].strMeal);
+//     }
+//     logMovies();
+//   });
+// });
+
+categoriesMenu.forEach((e) => {
   e.addEventListener("click", (e) => {
-    const variable = e.currentTarget.classList[1];
-    async function logMovies() {
-      const response = await fetch(
-        `https://www.themealdb.com/api/json/v1/1/filter.php?c=${variable}`
-      );
-      const movies = await response.json();
-      const renderedData = movies.meals.slice(0, 10);
-      console.log(renderedData);
-    }
-    logMovies();
+    const val = e.currentTarget.classList[1];
+    async function fetchData() {
+      try {
+        const response = await fetch(
+          `https://www.themealdb.com/api/json/v1/1/filter.php?c=${val}`
+        );
+        const data = await response.json();
+        const renderedData = data.meals.slice(0, 10);
 
-    document.querySelector(".result").innerText = variable;
+        const categoryResult = document.querySelector(".result");
+
+        categoryResult.textContent = val;
+        for (let i = 0; i < renderedData.length; i++) {
+          const element = renderedData[i];
+
+          const mainDiv = document.querySelector(".categories");
+          const secDiv = document.createElement("div");
+          secDiv.classList.add("categ-choices");
+          const header = document.createElement("h5");
+          header.textContent = element.strMeal;
+          const img = document.createElement("img");
+          img.src = element.strMealThumb;
+
+          secDiv.appendChild(img);
+          secDiv.appendChild(header);
+          mainDiv.appendChild(secDiv);
+        }
+      } catch (error) {
+        console.error("erro fetching data", error);
+      }
+    }
+    fetchData();
   });
 });
